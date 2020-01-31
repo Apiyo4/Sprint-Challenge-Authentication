f2749f95073c466f21ff7ce39f4a9b5e854f27a1
Annette Odingo <odingoannette@gmail.com>
2020-01-31 08:59:04 +0000

    Test to ensure user added exists

--- users/usersModel.spec.js
+++ users/usersModel.spec.js
@@ -1,22 +1,31 @@
 const db = require("../database/dbConfig");
 const Users = require("./usersModel");
 
-// beforeEach(async () => {
-//     await db('users').truncate()
-//   })
+beforeEach(async () => {
+    await db('users').truncate()
+  })
 
 describe("UsersModel", () => {
   it("Uses testing environment for DB_ENV", () => {
     expect(process.env.DB_ENV).toBe("testing");
   });
 
-  describe('insert()', ()=>{
-    it('inserts 2 users', async()=>{
-       await Users.insert({username:'admin1', password:"1234"})
-       await Users.insert({username:'admin2', password:"1234"})
-       const users = await db('users');
-       expect(users).not.toHaveLength(1);
-    })
-    
-})
+  describe("insert()", () => {
+    it("inserts 2 users", async () => {
+      await Users.insert({ username: "admin1", password: "1234" });
+      await Users.insert({ username: "admin2", password: "1234" });
+      const users = await db("users");
+      expect(users).not.toHaveLength(1);
+    });
+    it("ensures user inserted exists", async () => {
+      const user = await Users.insert({
+        username: "admin1",
+        password: "1234"
+      });
+      expect(user).toMatchObject({
+        username: "admin1",
+        password: "1234"
+      });
+    });
+  });
 });
